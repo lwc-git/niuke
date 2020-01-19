9e429f43b16c568382a7a048aae2d570cedec8f9
lwc <1327987604@qq.com>
2020-01-19 15:18:50 +0800

    nothing

--- src/leetcode/lc_451.java
+++ src/leetcode/lc_451.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 /**
@@ -8,6 +11,24 @@ import java.util.TreeMap;
  */
 public class lc_451 {
     public String frequencySort(String s) {
-        return null;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character,Integer>> queue = new PriorityQueue<>((v1,v2)->v2.getValue()-v1.getValue());
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            queue.offer(entry);
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!queue.isEmpty()){
+            Map.Entry<Character,Integer> entry = queue.poll();
+            char key = entry.getKey();
+            int value = entry.getValue();
+            while(value > 0){
+                sb.append(key);
+                value--;
+            }
+        }
+        return sb.toString();
     }
 }
