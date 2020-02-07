5d87526a1bd8e82362bcf2787789750ddcd887e8
lwc <1327987604@qq.com>
2020-02-07 11:53:53 +0800

    nothing

---
+++ src/leetcode/lc_457.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * @author lwc
+ * @date 2020/2/2 14:42
+ */
+public class lc_457 {
+    public boolean circularArrayLoop(int[] nums) {
+        int len = nums.length;
+        if(len < 2) {
+            return false;
+        }
+        int cur = 0;
+        HashSet<Integer> set = new HashSet<>();
+        while(! set.contains(cur)){
+            set.add(cur);
+            cur = (cur + nums[cur]) % len;
+            if(cur < 0) cur += len;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new lc_457().circularArrayLoop(new int[]{2,-1,1,-2,-2}));
+    }
+}
